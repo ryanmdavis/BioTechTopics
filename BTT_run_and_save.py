5ad857d0b6a2d7a706633e711bf1f89f1d5befee
Ryan Davis <ryan.m.davis5@gmail.com>
2018-03-27 06:18:43 -0700

    rebuild

--- BTT_run_and_save.py
+++ BTT_run_and_save.py
@@ -19,11 +19,7 @@ if __name__ == '__main__':
     # train and save tfidf representation: 20 minutes
     print('\nTraining tf-idf Vectorizer')
     start = time.time()
-<<<<<<< HEAD
-    tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenizeAndStemStrings, stop_words='english',ngram_range=(1,2), use_idf=True, smooth_idf = False, norm=None, min_df=0.002, max_df=0.2)    
-=======
     tfidf_vectorizer = TfidfVectorizer(tokenizer=tokenizeAndStemStrings, stop_words='english',ngram_range=(1,1), use_idf=True, smooth_idf = False, norm=None, min_df=0.002, max_df=0.2)    
->>>>>>> 08889f49ce4f5aef81cbfd3442d69fd20d61346e
     tfidf = tfidf_vectorizer.fit_transform(t.text_df['text_body'].apply(cleanString))
     end = time.time()
     print('Done training after ' + str(end-start) + ' seconds')
